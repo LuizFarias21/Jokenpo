dc2a367ba9b3b4252adcaca472d66ff5b208f8a6
Luiz Claudio <luizbfarias21@gmail.com>
2022-01-11 18:32:29 -0300

    update

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const body = document.querySelector('body');
+const section = document.querySelector('section');
+
 let playerScore = document.querySelector('.player.score');
+playerScore.style.transition = '0.3s';
+
 let computerScore = document.querySelector('.computer.score');
+computerScore.style.transition = '0.3s';
+
 let result = document.querySelector('.result');
+
 let boxPlayer = document.querySelector('.player-choice')
 let boxComputer = document.querySelector('.computer-choice');
+
+const audio = document.querySelector('audio');
 
 let playerScoreValue = 0;
 let computerScoreValue = 0;
@@ -16,11 +24,9 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
 
-    console.log(`${playerSelection} Vs ${computerSelection}`);
-
     boxPlayer.innerHTML = `<i class="fas fa-hand-${playerSelection} medium-i">`;
     boxComputer.innerHTML = `<i class="fas fa-hand-${computerSelection} medium-i">`;
-    // CÓDIGO DAS ESCOLHAS VEM AQUI
+
     if (playerSelection == 'rock' && computerSelection == 'paper'
         || playerSelection == 'paper' && computerSelection == 'scissors'
         || playerSelection == 'scissors' && computerSelection == 'rock') {
@@ -41,46 +47,84 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
-/* function game() {
-
-    for (let i = 1; i <= 5; i++) {
-
-        const playerSelection = prompt('Choose rock, paper or scissor:');
-        const computerSelection = computerPlay();
-
-        playRound(playerSelection, computerSelection);
-    }
-
-    console.log('=======================================================');
-
-    if (playerScore > computerScore) {
-        playerScore, computerScore = 0;
-        return 'PLAYER WINS 🏆️';
-    } else {
-        playerScore, computerScore = 0;
-        return 'COMPUTER WINS 🏆️';
-    }
-} */
-
 const btnRock = document.querySelector('.circle-btn.rock');
 const btnPaper = document.querySelector('.circle-btn.paper');
 const btnScissor = document.querySelector('.circle-btn.scissor');
-const score = document.querySelector('.score');
 
 btnRock.addEventListener('click', () => {
+    audio.play();
     const playerSelection = 'rock';
     const computerSelection = computerPlay();
     playRound(playerSelection, computerSelection);
+    gameResult()
+
 });
 
 btnPaper.addEventListener('click', () => {
+    audio.play();
     const playerSelection = 'paper';
     const computerSelection = computerPlay();
     playRound(playerSelection, computerSelection);
+    gameResult()
 });
 
 btnScissor.addEventListener('click', () => {
+    audio.play();
     const playerSelection = 'scissors';
     const computerSelection = computerPlay();
     playRound(playerSelection, computerSelection);
+    gameResult()
 });
+
+function restart() {
+
+    toggleBtnStatus(true);
+
+    const restartBtn = document.createElement('button');
+    restartBtn.className = 'restart-btn';
+    restartBtn.textContent = 'RESTART';
+    restartBtn.style.cursor = 'pointer';
+    section.append(restartBtn);
+
+    restartBtn.addEventListener('click', () => {
+        audio.play();
+        playerScore.style.color = 'white';
+        computerScore.style.color = 'white';
+        restartBtn.remove()
+        toggleBtnStatus(false);
+
+        playerScore.textContent = playerScoreValue;
+        computerScore.textContent = computerScoreValue;
+        result.textContent = 'Choose yours!'
+    });
+}
+
+function gameResult() {
+    if (playerScoreValue === 5 || computerScoreValue === 5) {
+
+        if (playerScoreValue > computerScoreValue) {
+            playerScoreValue = 0;
+            computerScoreValue = 0;
+            playerScore.style.color = '#29bb24';
+            computerScore.style.color = '#F62C2C';
+            result.textContent = 'PLAYER WINS 🏆️';
+            result.style.fontWeight = 'bold';
+
+
+        } else {
+            playerScoreValue = 0;
+            computerScoreValue = 0;
+            computerScore.style.color = '#29bb24';
+            playerScore.style.color = '#F62C2C';
+            result.textContent = 'COMPUTER WINS 🏆️';
+            result.style.fontWeight = 'bold';
+        }
+        restart();
+    }
+}
+
+function toggleBtnStatus(value) {
+    btnRock.disabled = value;
+    btnPaper.disabled = value;
+    btnScissor.disabled = value;
+}
